ade1f837700ee4ac46ab3a24545c61fdaba42579
blufox21 <lunevm@list.ru>
2025-11-02 03:20:17 +0300

    w320h170

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import time
 
 def main():
     try:
-        display = ST7789(w=320, h=170)
+        display = ST7789(w=170, h=320)
         colors = [0xf800, 0xfce0, 0xffe0, 0x07e0, 0x023f, 0x001f, 0xf81f]
         i = 0
         while(i<7):
